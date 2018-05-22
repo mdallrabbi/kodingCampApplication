278748712fee4236436fc41fd6438103193f6080
mdallrabbi <dreamer.rabbi@gmail.com>
2018-05-22 21:05:31 +0600

    AdminSite Permission

--- auth_module/admin.py
+++ auth_module/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from django.contrib.auth.models import Permission
 
 # Register your models here.
+#Reffereral reg. form Permssion
+
 admin.site.register(Permission)
